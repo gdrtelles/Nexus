038dd3f5f0b7dbec179df943dca2dec53c87a9ff
gdrtelles <rctelles@ucsc.edu>
2013-10-31 16:38:20 -0700

    beginning prototype to nexus
    has a bunch of useless stuff right now and can only place elements no
    actual gameplay

---
+++ Assets/raycast.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class raycast : MonoBehaviour
+
+{
+	public Texture oxygen;
+	public Texture nitrogen;
+	public Texture carbon;
+	public Texture hydrogen;
+
+	RaycastHit myhit = new RaycastHit();
+
+    Ray myray = new Ray();
+
+    void Update()
+
+    {
+
+        myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(myray, out myhit, 1000.0f) && Input.GetMouseButtonDown(0) && Input.GetKey ("o"))
+		{
+			myhit.collider.renderer.material.mainTexture = oxygen;
+			
+		}
+		
+		else if (Physics.Raycast(myray, out myhit, 1000.0f) && Input.GetMouseButtonDown(0) && Input.GetKey ("n"))
+		{
+			myhit.collider.renderer.material.mainTexture = nitrogen;
+			
+		}
+		else if (Physics.Raycast(myray, out myhit, 1000.0f) && Input.GetMouseButtonDown(0) && Input.GetKey ("c"))
+		{	
+			
+			myhit.collider.renderer.material.mainTexture = carbon;
+			
+		}
+		
+		else if (Physics.Raycast(myray, out myhit, 1000.0f) && Input.GetMouseButtonDown(0) && Input.GetKey ("h"))	
+		{
+			
+			myhit.collider.renderer.material.mainTexture = hydrogen;
+			
+		}
+		
+            
+
+    }
+
+}
---
+++ Assets/revert.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class revert : MonoBehaviour {
+
+	 public GameObject hex_tile;
+	 public Texture original;
+	 int lifespan = 3;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonUp(0) && lifespan != 0 && (Input.GetKey ("o") || Input.GetKey ("n") || Input.GetKey ("h") || Input.GetKey ("c")))
+		{
+            lifespan--;
+		    
+		}
+		else if(lifespan == 0)
+		{
+			hex_tile.renderer.material.mainTexture = original;
+			lifespan = 3;
+			
+		} 
+		
+	
+	}
+}
